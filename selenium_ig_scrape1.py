e0869356ad0a2bd2bb6a567ec89be7b5be3f536c
unknown <luke0545@gmail.com>
2020-01-20 21:44:48 -0600

    added text in output terminal

--- selenium_ig_scrape1.py
+++ selenium_ig_scrape1.py
@@ -14,9 +14,15 @@ driver.implicitly_wait(30)
 driver.get(url)
 
 followers = driver.find_element_by_xpath("""/html/body/div[1]/section/main/div/header/section/ul/li[2]/a/span""")
-print(followers.text)
+
+print ("The Rock has {} followers on Instagram".format(followers.text))
 
 jumunji_link = driver.find_element_by_xpath("""/html/body/div[1]/section/main/div/header/section/div[2]/a[1]""").click()
+
+closest_theater = driver.find_element_by_xpath("""/html/body/div[2]/div/div[1]/div[1]/div/div[2]/ul/li[1]/div[1]/div[2]/h3""")
+
+print (closest_theater.text)
+
 
 #posts = driver.find_element_by_class_name("g47SY ")
 #python_button.click() #click fhsu link
